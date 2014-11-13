f8643218cececfea507be6f56999963cdec617d6
Kirill Klenov <horneds@gmail.com>
2014-11-13 18:50:03 +0300

    Worsest project

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import unused_import
+
+
+def main():
+    unused_variable = 1
+    pass
+    return None
+
+    test += 1
+
+if __name__ == "__main__":
+    main()
+
